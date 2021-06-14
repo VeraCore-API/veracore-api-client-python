65bb58b52a5a720e9b5b80338c8a6addce7b3dd7
Eli Keimig <eli.keimig@dataprivia.com>
2021-06-14 08:33:17 -0400

    updated versioning and added deployment

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Eli Keimig'
 author = 'Eli Keimig'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.2'
+release = '0.0.3.alpha1'
 
 
 # -- General configuration ---------------------------------------------------
